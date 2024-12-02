82dddb97517d3705bdf6706000538a463ff7a3f0
Cyrus-0101 <cyrus_g@outlook.com>
2024-12-02 10:07:53 +0300

    chore: Refactor experience plane

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,6 +1,14 @@
-import { Float, MeshReflectorMaterial, PresentationControls, Stage } from "@react-three/drei";
+import {
+  MeshReflectorMaterial,
+  PresentationControls,
+  Stage,
+} from "@react-three/drei";
+
+import { Suspense } from "react";
+import Chair from "./Chair";
 
 const Experience = () => {
+
   return (
     <PresentationControls
       speed={1.5}
@@ -8,33 +16,25 @@ const Experience = () => {
       zoom={0.7}
       polar={[-0.1, Math.PI / 4]}
     >
-      <Stage environment={"city"} intensity={0.6}>
-        <Float
-            speed={1}
-            rotationIntensity={1}
-            floatIntensity={1}
-            floatingRange={[0, 1]}
-        >
-            <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-            </mesh>
-        </Float>
+      <Stage environment={"studio"} intensity={0.6}>
+        <Suspense fallback={null}>
+            <Chair />
+        </Suspense>
       </Stage>
       <mesh rotation={[-Math.PI / 2, 0, 0]}>
         <planeGeometry args={[170, 170]} />
         <MeshReflectorMaterial
-            blur={[300, 100]}
-            resolution={2048}
-            mixBlur={1}
-            mixStrength={40}
-            roughness={1}
-            depthScale={1.2}
-            minDepthThreshold={0.4}
-            maxDepthThreshold={1.4}
-            color={"#101010"}
-            metalness={0.5}
-            mirror={0}
+          blur={[300, 100]}
+          resolution={2048}
+          mixBlur={1}
+          mixStrength={40}
+          roughness={1}
+          depthScale={1.2}
+          minDepthThreshold={0.4}
+          maxDepthThreshold={1.4}
+          color={"#101010"}
+          metalness={0.5}
+          mirror={0}
         />
       </mesh>
     </PresentationControls>
