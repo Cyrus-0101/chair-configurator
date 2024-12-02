59e4191838e1a5afc840edd7cb0f774dfd9f1a23
Cyrus-0101 <cyrus_g@outlook.com>
2024-12-02 10:11:02 +0300

    chore: Wrapped our app in a customization provider and passed our Configurator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { Canvas } from '@react-three/fiber'
 import './App.css'
 import Experience from './components/Experience'
+import Configurator from './components/Configurator'
+import { CustomizationProvider } from './providers/CustomizationProvider'
 
 function App() {
 
   return (
     <>
-      <div className='app'>
-        <Canvas>
-          <color attach="background" args={["#213547"]} />
-          <Experience />
-        </Canvas>
-      </div>
+      <CustomizationProvider>
+        <div className='app'>
+          <Canvas>
+            <color attach="background" args={["#213547"]} />
+            <Experience />
+          </Canvas>
+          <Configurator />
+        </div>
+      </CustomizationProvider>
     </>
   )
 }
