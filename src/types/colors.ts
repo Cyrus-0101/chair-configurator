6a1bb81f824086fccff4b1ce42daa7c137620364
Cyrus-0101 <cyrus_g@outlook.com>
2024-12-02 10:10:04 +0300

    chore; Created color constants and types

---
+++ src/types/colors.ts
@@ -0,0 +1,64 @@
+export type ChairColor = {
+  color: string;
+  name: string;
+}
+
+export type CushionColor = {
+  color: string;
+  name: string;
+}
+
+export const chairColors: ChairColor[] = [
+  {
+    color: "#683434",
+    name: "brown",
+  },
+  {
+    color: "#1a5e1a",
+    name: "green",
+  },
+  {
+    color: "#659994",
+    name: "blue",
+  },
+  {
+    color: "#996599",
+    name: "mauve",
+  },
+  {
+    color: "#ffa500",
+    name: "orange",
+  },
+  {
+    color: "#555558",
+    name: "gray",
+  },
+]
+
+export const cushionColors: CushionColor[] = [
+  {
+    color: "#683434",
+    name: "brown",
+  },
+  {
+    color: "#1a5e1a",
+    name: "green",
+  },
+  {
+    color: "#659994",
+    name: "blue",
+  },
+  {
+    color: "#996599",
+    name: "mauve",
+  },
+  {
+    color: "#ffa500",
+    name: "orange",
+  },
+  {
+    color: "#555558",
+    name: "gray",
+  },
+]
+
